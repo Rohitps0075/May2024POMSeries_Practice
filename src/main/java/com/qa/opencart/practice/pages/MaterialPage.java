db6dd51d5759616d1384477f3335b32e0fd19d91
Rohit <rohitps008gmail.com>
2026-04-16 19:53:44 +0530

    added new material page

---
+++ src/main/java/com/qa/opencart/practice/pages/MaterialPage.java
@@ -0,0 +1,15 @@
+package com.qa.opencart.practice.pages;
+
+public class MaterialPage {
+
+	String materialName="Hard Disk";
+	
+	public void cost()
+	{
+		System.out.println("cost of the all materials are above 100 and below 500");
+	}
+	
+	
+	
+	
+}
